61ebcd53ca63d7d6e920f4b22c111d4dd5322080
Admin <Admin@DESKTOP-BEICT6B>
2024-01-20 16:50:52 +0530

    test commit

---
+++ Myjava/src/opearators_arithmetics/operators.java
@@ -0,0 +1,22 @@
+package opearators_arithmetics;
+
+public class operators {
+
+	public static void main(String[] args) {
+		//--> mod(%) and division
+		
+				/*float a = 10.0f, b = 3.0f;  		
+				float c = a/b;
+				float m = a%b;
+				System.out.println(c);
+				System.out.println(m);*/
+				
+				int a = 10, b = 3;  		
+				int c = a/b;
+				int m = a%b;
+				System.out.println(c);
+				System.out.println(m);
+
+	}
+
+}
