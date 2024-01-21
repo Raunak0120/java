457579c0b25383a70b8c2d31c564f9b961b822b0
Admin <Admin@DESKTOP-BEICT6B>
2024-01-21 12:10:44 +0530

    Area and Volume of cubiod

---
+++ Myjava/src/opearators_arithmetics/Area_and_Volume_of_cubiod_.java
@@ -0,0 +1,17 @@
+package opearators_arithmetics;
+import java.util.Scanner;
+public class Area_and_Volume_of_cubiod_ {
+	public static void main(String[] args) {
+		float l,b,h;
+		double a,v;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Length, Breadth, Height");
+		l = sc.nextFloat();
+		b = sc.nextFloat();
+		h = sc.nextFloat();
+		a = 2*((l*b)+(b*h)+(l*h));
+		v = l*b*h;
+		System.out.println("Area is" +a+ " " +v);
+	}
+
+}
