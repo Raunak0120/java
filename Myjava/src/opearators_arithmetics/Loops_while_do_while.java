0150325a4651fb013e4cfc75818a920e37416ae7
Admin <Admin@DESKTOP-BEICT6B>
2024-02-06 12:11:48 +0530

    modified loop added for also

--- Myjava/src/opearators_arithmetics/Loops_while_do_while.java
+++ Myjava/src/opearators_arithmetics/Loops_while_do_while.java
@@ -14,20 +14,39 @@ public class Loops_while_do_while {
 		
 		//squares of 2 using do while loop
 		
-		int i = 1, n = 100;
+		/*int i = 1, n = 100;
 		do {
 			System.out.println(i);
 			i = i * 2;
 		}
-		while(i<n);
+		while(i<n);*/
 		
 		//infinite loop
 			
 		/*int i = 1;
 		while(true) {
 			System.out.println(i);
-			i++;*/
+			i++;
+		}*/
+		
+		// Printing 0-10 using for loop 
+		
+		/*for (int i = 0; i < 11; i++) {
+			System.out.print(i+"\t");
+			
+		}System.out.println();
+		//printing reverse 10-0 using for loop
+		
+		for (int i = 10; i >= 0; i--) {
+			System.out.print(i+"\t");
+		}*/
+		
+		// double variable in for loop
+		
+		for (int i=0,j=1; i<=10; i++,j=j*2) {
+			System.out.println("2^"+i+"\s=\s"+j);
 		}
+	
 		
 	}
 
