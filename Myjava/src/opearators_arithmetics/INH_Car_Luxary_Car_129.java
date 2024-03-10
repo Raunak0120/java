6af407815399005d638567bf898c16bcd2fc4803
Admin <Admin@DESKTOP-BEICT6B>
2024-03-10 10:54:23 +0530

    INH_Car_Luxary_Car_129

---
+++ Myjava/src/opearators_arithmetics/INH_Car_Luxary_Car_129.java
@@ -0,0 +1,39 @@
+package opearators_arithmetics;
+
+	class car{
+		public void start() {
+			System.out.println("Car is Started");
+		}
+		public void accelerate() {
+			System.out.println("Car is Acclerated");
+		}
+		public void changeGear() {
+			System.out.println("Gear is Changed");
+		}
+	}
+	class luxaryCar{
+		public void changeGear() {
+			System.out.println("Automatic Gear");
+		}
+		public void openRoof() {
+			System.out.println("Sun Roof is Opened");
+		}
+	}
+
+public class INH_Car_Luxary_Car_129 {
+
+	public static void main(String[] args) {
+		car c = new car();
+		c.start();
+		c.accelerate();
+		c.changeGear();
+		
+		System.out.print("\n");
+		
+		luxaryCar lc =new luxaryCar();
+		lc.changeGear();
+		lc.openRoof();
+
+	}
+
+}
